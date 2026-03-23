30292311c669296af45693fa5e44e70284b45b51
Mack Ott <mack.ott@asml.com>
2026-03-23 10:13:27 +0100

    Adding the ability to control the limit for the debug databricks catalog IO manager

--- libraries/dagster-unity-catalog-polars/dagster_unity_catalog_polars/debug_unity_catalog_iomanager.py
+++ libraries/dagster-unity-catalog-polars/dagster_unity_catalog_polars/debug_unity_catalog_iomanager.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections.abc import Callable
 from functools import wraps
 
 from .unity_catalog_iomanager import DatabricksUnityCatalogInputManager
@@ -7,6 +8,24 @@ from .unity_catalog_iomanager import DatabricksUnityCatalogInputManager
 
 class DebugDatabricksUnityCatalogInputManager(DatabricksUnityCatalogInputManager):
     """Overwrites the UnityCatalogInputManager such that it adds LIMIT 10 to the original query"""
+
+    def __init__(
+        self,
+        token_generator: Callable[[], str],
+        server_hostname: str,
+        endpoint: str,
+        limit: int = 10_000,
+    ):
+        """Initializes the UnityCatalogInputManager.
+
+        Args:
+            token_generator (Callable): Non-cached token generator callable object, to be called when loading input
+            server_hostname (str): The hostname of the databricks environment
+            endpoint (str): The Databricks SQL warehouse endpoint to connect
+            limit (int): Maximum number of rows to grab
+        """
+        super().__init__(token_generator, server_hostname, endpoint)
+        self.limit = limit
 
     @wraps(DatabricksUnityCatalogInputManager.form_query)
     def form_query(  # type: ignore
@@ -21,5 +40,5 @@ class DebugDatabricksUnityCatalogInputManager(DatabricksUnityCatalogInputManager
         """Adds a LIMIT to the original query"""
         return (
             super().form_query(catalog, schema, table, columns, predicate, partition_predicate)
-            + " LIMIT 10000"
+            + f" LIMIT {self.limit}"
         )  # type: ignore
